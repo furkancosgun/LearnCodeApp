7ac6f3765f0654829d8f74aa494b2d0a5a4c0341
Furkan COSGUN <furkan51cosgun@gmail.com>
2023-01-16 20:51:35 +0300

    Initial Commit

---
+++ LearnCodeApp/LearnCodeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnCodeAppApp.swift
+//  LearnCodeApp
+//
+//  Created by Furkan on 16.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct LearnCodeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
