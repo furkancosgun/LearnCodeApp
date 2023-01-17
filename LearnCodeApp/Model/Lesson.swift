09ffe0a87a934c052c3a089214329ac486ec0ca4
Furkan COSGUN <furkan51cosgun@gmail.com>
2023-01-17 22:17:38 +0300

    Lesson Model

---
+++ LearnCodeApp/Model/Lesson.swift
@@ -0,0 +1,16 @@
+//
+//  Lesson.swift
+//  LearnCodeApp
+//
+//  Created by Furkan on 17.01.2023.
+//
+
+import Foundation
+
+struct Lesson{
+    var id : Int
+    var title : String
+    var subTitle : String
+    var content : String
+    var language : String
+}
