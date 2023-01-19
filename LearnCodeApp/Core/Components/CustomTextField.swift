b6dffd4d9ba97378ffe46ecd0e75bef0ed9e99bb
Furkan COSGUN <furkan51cosgun@gmail.com>
2023-01-19 20:31:07 +0300

    CustomView

--- LearnCodeApp/Core/Components/CustomTextField.swift
+++ LearnCodeApp/Core/Components/CustomTextField.swift
@@ -11,13 +11,19 @@ struct CustomTextField: View {
     @Binding var text : String
     var title : String
     var icon : String
+    var isSecure : Bool
     var body: some View {
         VStack(alignment: .leading){
             HStack{Spacer()}
             Text(title).bold().foregroundColor(.white)
             HStack {
               Image(systemName: icon).foregroundColor(.black)
-                TextField(title, text: $text).padding(10).foregroundColor(.black)
+                if isSecure {
+                    SecureField(title,text: $text).padding(10).foregroundColor(.black)
+                }else{
+                    TextField(title, text: $text).padding(10).foregroundColor(.black)
+                }
+                
               }
             .padding(10)
             .background(.white)
@@ -27,8 +33,10 @@ struct CustomTextField: View {
     }
 }
 
-struct CustomTextField_Previews: PreviewProvider {
-    static var previews: some View {
-        CustomTextField(text: .constant(""), title: "login",icon: "person")
-    }
-}
+/*
+ struct CustomTextField_Previews: PreviewProvider {
+ static var previews: some View {
+ CustomTextField(text: .constant(""), title: "login",icon: "person")
+ }
+ }
+ */
