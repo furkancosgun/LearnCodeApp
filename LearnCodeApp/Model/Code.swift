aac3405e0d5b98405a92b43583843a53aade9328
Furkan COSGUN <furkan51cosgun@gmail.com>
2023-01-19 20:29:45 +0300

    Code Model

---
+++ LearnCodeApp/Model/Code.swift
@@ -0,0 +1,13 @@
+//
+//  Code.swift
+//  LearnCodeApp
+//
+//  Created by Furkan on 19.01.2023.
+//
+
+import Foundation
+
+struct Code : Decodable{
+    var language : String
+    var code :String
+}
